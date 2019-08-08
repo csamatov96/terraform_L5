31eb2f3dde6f705464c5ff516cc40a24f5393621
csamatov96 <996bkg@gmail.com>
2019-08-07 19:44:50 -0500

    changes

--- sec_group.tf
+++ sec_group.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "jenkins" {
   name        = "jenkins"
   description = "Allow inbound traffic"
-  vpc_id      = "${var.vpc}" #default one 
+  vpc_id      = "${var.vpc}"
 
   ingress {
     from_port   = 22
@@ -16,17 +16,18 @@ resource "aws_security_group" "jenkins" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    
+
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-    }
-  tags ={
-      Name ="${var.Name}.public"
-      Env ="${var.Env}"
-      Created_by ="${var.Created_by}"
-      Dept ="${var.Dept}"
+  }
+
+  tags = {
+      Name = "${var.Name}.public"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
   }
 }
