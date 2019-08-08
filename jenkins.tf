4224050b651eebf9d86be13abb2767b10b64ced9
csamatov96 <996bkg@gmail.com>
2019-08-07 19:10:40 -0500

    sent

---
+++ jenkins.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "jenkins" { #1. creating an actual instance 
+    ami = "${var.ami}"
+    instance_type = "${var.instance_type}"
+    key_name = "${aws_key_pair.developer_key.name}"
+    associate_public_ip_address = true
+    security_groups = ["${aws_security_group.jenkins.id}"] #id or name ???
+
+    #2. establishing a connection
+    provisioner "remote-exec" {  
+        connection = {
+            host = "${self.public_ip}"
+            type = "ssh" #or 
+            user = "${var.user}" #ec2-user 
+            private_key = "${file("~/.ssh/id_rsa")}" #use that key whenever it creates an instance 
+        }
+
+        #3. running an actual tasks
+        inline = [ #https://linuxize.com/post/how-to-install-jenkins-on-centos-7/
+            "sudo yum install java-1.8.0-openjdk-devel",
+            "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
+            "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
+            "sudo yum install jenkins",
+            "sudo systemctl start jenkins"
+        ]
+    }
+}
---
+++ route53.tf
@@ -0,0 +1,8 @@
+#https://www.terraform.io/docs/providers/aws/r/route53_record.html
+resource "aws_route53_record" "jenkins33" {
+  zone_id = "${var.zone_id}"
+  name    = "jenkins33.${var.domain}"
+  type    = "A"
+  ttl     = "300"
+  record = ["${aws_instance.jenkins33.public_ip}"]
+}
