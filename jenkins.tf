df6ad2f4dd0326674fd1d8b2c65cf5f60b287719
csamatov96 <996bkg@gmail.com>
2019-08-07 19:32:55 -0500

    -y

--- jenkins.tf
+++ jenkins.tf
@@ -7,7 +7,6 @@ resource "aws_instance" "jenkins" { #1. creating an actual instance
 
     #2. establishing a connection
     provisioner "remote-exec" {  
-        
         connection = {
             host = "${self.public_ip}"
             type = "ssh" #or 
@@ -16,8 +15,9 @@ resource "aws_instance" "jenkins" { #1. creating an actual instance
         }
 
         #3. running an actual tasks
-        inline = [ #https://linuxize.com/post/how-to-install-jenkins-on-centos-7/
-            "sudo yum install java-1.8.0-openjdk-devel",
+        #https://linuxize.com/post/how-to-install-jenkins-on-centos-7/
+        inline = [ 
+            "sudo yum install java-1.8.0-openjdk-devel -y",
             "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
             "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
             "sudo yum install jenkins",
