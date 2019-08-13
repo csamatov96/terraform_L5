8d6047660c2228c9c50e34b90e3e7781c9911c73
csamatov96 <996bkg@gmail.com>
2019-08-13 01:27:26 -0500

    changes

--- jenkins/backend.tf
+++ jenkins/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" { #
         region = "us-east-1" #
-        bucket = "remote-exec-chyngyzkan" ##
-        key = "remote.state"
+        bucket = "remote-exec-chyngyzkan" #
+        key = "remote.state" #
     }
 }
